d5925211b30e2dec81bdd3b00321f53833f4fdb8
Saria Islam <saraaaay@gmail.com>
2017-06-14 14:05:51 -0700

    Refactor play/pause button using jQuery and fix syntax errors

--- scripts/album.js
+++ scripts/album.js
@@ -40,15 +40,54 @@ var createSongRow = function(songNumber, songName, songLength) {
     +   '</td>'
     ;
     
-    return $(template);
+    var $row = $(template);
+    
+    var clickHandler = function() {
+        var songItem = $(this).find('.song-item-number');
+        
+        if (currentlyPlayingSong === null) {
+            songItem.html(pauseButtonTemplate);
+            currentlyPlayingSong = songItem.attr('data-song-number');
+        } else if (songItem.html(playButtonTemplate) === songItem.attr('data-song-number')) {
+                songItem.html(playButtonTemplate);
+                currentlyPlayingSong = null;
+                   } else if (songItem.html(playButtonTemplate) !== songItem.attr('data-song-number')) {
+                       var currentlyPlayingSongElement = $('[data-song-number="' + currentlyPlayingSong + '"]');
+                       currentlyPlayingSongElement.html(currentlyPlayingSongElement.attr('data-song-number'));
+                       songItem.html(pauseButtonTemplate);
+                       currentlyPlayingSong = songItem.attr('data-song-number');
+                   }
+    };
+    
+    var onHover = function(event) {
+        var songItem = $(this).find('.song-item-number');
+        
+        if (songItem.attr('data-song-number') != currentlyPlayingSong) {
+            songItem.html(playButtonTemplate);
+        }
+    };
+    var offHover = function(event) {
+        var songItem = $(this).find('.song-item-number');
+        
+        var songItemNumber = songItem.attr('data-song-number');
+        
+        if (songItemNumber != currentlyPlayingSong) {
+            songItem.html(songItemNumber);
+        }
+    };
+    
+    $row.find('.song-item-number').click(clickHandler);
+    $row.hover(onHover, offHover);
+    
+    return $row;
 };
 
 var setCurrentAlbum = function(album) {
     var $albumTitle = $('.album-view-title');
     var $albumArtist = $('.album-view-artist');
     var $albumReleaseInfo = $('.album-view-release-info');
-    var $albumImage = ('.album-cover-art');
-    var $albumSongList = $(.album-view-song-list);
+    var $albumImage = $('.album-cover-art');
+    var $albumSongList = $('.album-view-song-list');
     
     $albumTitle.text(album.title);
     $albumArtist.text(album.artist);
@@ -63,89 +102,12 @@ var setCurrentAlbum = function(album) {
     }
 };
 
-var findParentByClassName = function(element, targetClass) {
-    if (element) {
-        var currentParent = element.parentElement;
-        while (currentParent.className !== targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
-        }
-        return currentParent;
-    }
-};
-
-var getSongItem = function(element) {
-    switch (element.className) {
-        case 'album-song-button':
-        case 'ion-play':
-        case 'ion-pause':
-            return findParentByClassName(element, 'song-item-number');
-        case 'album-view-song-item':
-            return element.querySelector('.song-item-number');
-        case 'song-item-title':
-        case 'song-item-duration':
-            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-        case 'song-item-number':
-            return element;
-        default:
-            return;
-    }  
-};
-
-var clickHandler = function(targetElement) {
-    var songItem = getSongItem(targetElement);
-    
-    if (currentlyPlayingSong === null) {
-        songItem.innerHTML = pauseButtonTemplate;
-        currentlyPlayingSong = songItem.getAttribute('data-song-number');
-    } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-        songItem.innerHTML = playButtonTemplate;
-        currentlyPlayingSong = null;
-    } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-        var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-        currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-        songItem.innerHTML = pauseButtonTemplate;
-        currentlyPlayingSong = songItem.getAttribute('data-song-number');
-    }
-};
-
-// Elements to which we'll be adding listeners
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
-
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
 // Store state of playing songs
 var currentlyPlayingSong = null;
 
-window.onload = function() {
+$(document).ready(function() {
     setCurrentAlbum(albumPicasso);
-    
-    songListContainer.addEventListener('mouseover', function(event) {
-        // Only target individual song rows during event delegation
-        if (event.target.parentElement.className === 'album-view-song-item') {
-            // Change the content from the number to the play button's HTML
-            var songItem = getSongItem(event.target);
-            
-            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
-                songItem.innerHTML = playButtonTemplate;
-            }
-        }
-    });
-    
-    for (var i = 0; i < songRows.length; i++) {
-        songRows[i].addEventListener('mouseleave', function(event) {
-            var songItem = getSongItem(event.target);
-            var songItemNumber = songItem.getAttribute('data-song-number');
-            
-            if (songItemNumber !== currentlyPlayingSong) {
-                songItem.innerHTML = songItemNumber;
-            }
-        });
-        
-        songRows[i].addEventListener('click', function(event) {
-            // Event handler call
-            clickHandler(event.target);
-        });
-    }
-};
+});
